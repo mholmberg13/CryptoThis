d62f717f88bdeba3d7df4abd1c381dfa4fd830bf
Matt Holmberg <mholmberg13@gmail.com>
2020-02-02 20:58:21 -0700

    connected each button to api data, created and styled divs for cards

--- app.js
+++ app.js
@@ -4,9 +4,77 @@ console.log("working")
 
 $(() => {
 
-    const userInput = $("#search").val();
+    const retrieve = () => {
+        
+        
 
-    const search = () => {
+        $.ajax({
+            url:'https://api.coinlore.com/api/ticker/?id=90', 
+            type: 'GET',
+            
+            
+        }).then(
+            (response)=>{
+                console.log("searching")
+                console.log(response[0].name);
+                // $('#name').append(response[0].name)
+                // $('#price').append(response[0].price_usd)
+                // $('#rank').append(response[0].rank)
+                // $('#24change').append(response[0].percent_change_24h)
+            },
+            ()=>{
+                console.log('bad request');
+            }
+        );
+    
+    }
+
+    $('#ret').on('click', (event) => {
+
+        
+        event.preventDefault();
+
+        retrieve();        
+
+        
+    });
+    
+    const retrieve2 = () => {
+        
+        
+
+        $.ajax({
+            url:'https://api.coinlore.com/api/ticker/?id=80', 
+            type: 'GET',
+            
+            
+        }).then(
+            (response)=>{
+                console.log("searching")
+                console.log(response[0].name);
+                // $('#name').append(response[0].name)
+                // $('#price').append(response[0].price_usd)
+                // $('#rank').append(response[0].rank)
+                // $('#24change').append(response[0].percent_change_24h)
+            },
+            ()=>{
+                console.log('bad request');
+            }
+        );
+    
+    }
+
+    $('#ret2').on('click', (event) => {
+
+        
+        event.preventDefault();
+
+        retrieve2();        
+
+        
+    });
+
+    const retrieve3 = () => {
         
         
 
@@ -16,9 +84,13 @@ $(() => {
             
             
         }).then(
-            (data)=>{
+            (response)=>{
                 console.log("searching")
-                console.log(data);
+                console.log(response[0].name);
+                // $('#name').append(response[0].name)
+                // $('#price').append(response[0].price_usd)
+                // $('#rank').append(response[0].rank)
+                // $('#24change').append(response[0].percent_change_24h)
             },
             ()=>{
                 console.log('bad request');
@@ -26,13 +98,48 @@ $(() => {
         );
     
     }
-    
-    $('form').on('submit', (event) => {
+
+    $('#ret3').on('click', (event) => {
 
         
         event.preventDefault();
 
-        search();        
+        retrieve3();        
+
+        
+    });
+
+    const retrieve4 = () => {
+        
+        
+
+        $.ajax({
+            url:'https://api.coinlore.com/api/ticker/?id=33234', 
+            type: 'GET',
+            
+            
+        }).then(
+            (response)=>{
+                console.log("searching")
+                console.log(response[0].name);
+                // $('#name').append(response[0].name)
+                // $('#price').append(response[0].price_usd)
+                // $('#rank').append(response[0].rank)
+                // $('#24change').append(response[0].percent_change_24h)
+            },
+            ()=>{
+                console.log('bad request');
+            }
+        );
+    
+    }
+
+    $('#ret4').on('click', (event) => {
+
+        
+        event.preventDefault();
+
+        retrieve4();        
 
         
     });
